85426c526173890ecf5be474dc80bb18086e0b33
Jean-Denis Vauguet <jd@vauguet.fr>
2015-12-21 00:49:47 +0100

    [Refacto] A PostProcessing object as the toolchain
    Signed-off-by: Jean-Denis Vauguet <jd@vauguet.fr>

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ var path = require('path');
 var _ = require('lodash');
 var Q = require('q');
 
-var toolchain = require('./utils/toolchain');
+var PostProcessing = require('./lib/post_processing');
 
 module.exports = {
   hooks: {
     finish: function() {
       var book = this;
 
-      return Q()
-      .then(_.bind(toolchain.init, book))
-      .then(_.bind(toolchain.readConfiguration, book))
-      .spread(_.bind(toolchain.runPostProcesses, book));
+      var postProcessing = new PostProcessing(book);
+      return postProcessing.run();
     }
   }
 };
